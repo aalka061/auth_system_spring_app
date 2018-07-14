42e0efc72da3cb7bb3eba88f38a4328afa2e21ed
aalkaf <a_algg@hotmail.com>
2018-07-14 01:13:45 -0400

    login form defualt init

---
+++ src/main/java/com/login/spring/app/config/SecurityAppInitilizer.java
@@ -0,0 +1,9 @@
+package com.login.spring.app.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//register the spring security filters
+public class SecurityAppInitilizer extends AbstractSecurityWebApplicationInitializer {
+
+	
+}
